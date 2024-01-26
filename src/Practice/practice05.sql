3c4aa1c1af91e4521e0f3986731659e057a66c89
poula9744 <poula9744@naver.com>
2024-01-26 22:59:25 +0900

    6번부터 다시

--- src/Practice/practice05.sql
+++ src/Practice/practice05.sql
@@ -1,3 +1,4 @@
+
 /* 문제1.
 담당 매니저가 배정되어있으나 커미션비율이 없고, 월급이 3000초과인 직원의
 이름, 매니저아이디, 커미션 비율, 월급 을 출력하세요. (45건) */ 
@@ -21,7 +22,7 @@ select employee_id 직원번호
             ,first_name 이름
             ,salary 월급
             ,CASE when date_format(min(hire_date), '%a') = 'Mon' then date_format(min(hire_date), '%Y년 %m월 %d일(월요일)')
-					   when date_format(min(hire_date), '%a') = 'Tue' then date_format(min(hire_date), '%Y년 %m월 %d일(화요일)')
+					   when date_format(min(hire_date), '%a') = 'Tue' then date_format(min(hire_date), '%Y년 %m월 %d일 (화요일)')
                        when date_format(min(hire_date), '%a') = 'Wen' then date_format(min(hire_date), '%Y년 %m월 %d일 (수요일)')
                        when date_format(min(hire_date), '%a') = 'Thu' then date_format(min(hire_date), '%Y년 %m월 %d일 (목요일)')
                        when date_format(min(hire_date), '%a') = 'Fri' then date_format(min(hire_date), '%Y년 %m월 %d일 (금요일)')
@@ -46,23 +47,77 @@ order by salary desc;
 -매니저별 평균월급은 소수점 첫째자리에서 반올림 합니다.
 -출력내용은 매니저아이디, 매니저이름(first_name), 매니저별평균월급, 매니저별최소월급, 
 매니저별최대월급 입니다.  (9건) */
-select e1.manager_id
-			,e1.first_name
-            ,round(avg(e1.salary)) avg_salary
-            ,min(e1.salary) min_salary
-            ,max(e1.salary) max_salary
-from employees e1, (select manager_id, max(salary) max_salary
-								  from employees
-                                  group by manager_id) s
-                                  where  e1.hire_date >20050101
-group by e1.manager_id, e1.first_name, e1.hire_date                                
-having e1.manager_id = s.manager_id
-and max(e1.salary) = s.max_salary
 
-and round(avg(e1.salary)) >=5000
-
-
-order by avg(e1.salary) desc;
+select e1.manager_id 
+			,e2.first_name '매니저 이름'
+			,round(avg(e1.salary), 1)
+			,min(e1.salary)
+			,max(e1.salary)
+from employees e1
+inner join employees e2 
+	on e1.manager_id = e2.employee_id
+    
+where e1.hire_date >= 20050101  
+group by e1.manager_id, e2.first_name
+having round(avg(e1.salary),1) >=5000
+order by avg(e1.salary) desc
+;
 
 
 
+/*문제4.
+각 사원(employee)에 대해서 사번(employee_id), 이름(first_name), 부서명
+(department_name), 매니저(manager)의 이름(first_name)을 조회하세요.
+부서가 없는 직원(Kimberely)도 표시합니다.             (106명)        */
+select  e1.employee_id 사번
+            ,e1.first_name 이름
+            ,d.department_name 부서명
+            ,e2.first_name 매니저이름
+from employees e1
+left join departments d
+	on e1.department_id = d.department_id
+inner join employees e2
+	on e1.manager_id = e2.employee_id
+;
+
+
+/*문제5.
+2005년 이후 입사한 직원중에 입사일이 11번째에서 20번째의 직원의
+사번, 이름, 부서명, 월급, 입사일을 입사일 순서로 출력하세요 */
+select employee_id 
+			,first_name
+            ,department_name
+            ,salary
+            ,hire_date
+from employees e, departments d
+where e.department_id = d.department_id
+and hire_date >= 2005/01/01
+order by hire_date asc
+limit 10, 10;
+
+/*문제6.
+가장 늦게 입사한 직원의 이름(first_name last_name)과 월급(salary)과 근무하는 
+부서 이름(department_name)은? */
+select concat(first_name, ' ' , last_name) 이름
+           ,salary 월급
+           ,department_name 부서이름
+           ,e.hire_date
+from employees e, departments d
+where e.department_id = d.department_id
+order by hire_date desc
+limit 0,2
+;
+
+/*문제7.
+평균월급(salary)이 가장 높은 부서 직원들의 직원번호(employee_id), 이름(firt_name), 성
+(last_name)과 업무(job_title), 월급(salary)을 조회하시오. */
+select e.employee_id
+		    ,first_name
+            ,last_name
+            ,job_title
+            ,s.salary
+            ,avg(salary)
+from employees e, jobs j
+where e.job_id = j.job_id
+group by employee_id ,first_name ,last_name,job_title ,salary
+;									  
